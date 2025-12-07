1d1d082ae5c7799f6d552ec9a2ad989ea358785c
shastitko1970-netizen <shastitko1970@gmail.com>
2025-12-07 19:42:14 +0300

    фикс старта

---
+++ public/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'endora-web',
+    script: './node_modules/.bin/next',
+    args: 'start',
+    cwd: '/var/www/endora_web',
+    node_args: '--max-old-space-size=512',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    max_memory_restart: '512M',
+    autorestart: true,
+    watch: false
+  }]
+}
+EOF
